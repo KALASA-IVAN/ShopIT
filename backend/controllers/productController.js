9568775022b149ba1abfbedb41c76ee32ca69a9a
KALASA_IVAN <moikalasa12@gmail.com>
2021-11-30 08:38:58 +0200

    Handling receiption of products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 
-//Create new product =>/api/v1/product/new
+//create new product =>/api/v1/product/new
 
 exports.newProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
@@ -11,9 +11,14 @@ exports.newProduct = async (req, res, next) => {
   })
 }
 
-exports.getProducts = (req, res, next) => {
+//get all products => /api/v1/products
+exports.getProducts = async (req, res, next) => {
+  
+  const products = await Product.find();
+
   res.status(200).json({
     success: true,
-    message: "This route will show all the products in database."
+    count:products.length,
+    products
   });
 };
