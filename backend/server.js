7a2782a745520f3478e1f71581be1719d5ec9d83
KALASA_IVAN <moikalasa12@gmail.com>
2021-12-02 14:54:02 +0200

    Corrected Error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,21 @@ const connectDatabase = require("./config/database");
 // setting up config file
 dotenv.config({ path: "backend/config/config.env" });
 
-
 // connecting to database
 
-connectDatabase();  
-    
-app.listen(process.env.PORT, () => {
+connectDatabase();
+
+const server = app.listen(process.env.PORT, () => {
   console.log(
     `Server started on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
   );
+});
+
+//Handling unhandled promise rejection warnings
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR:${err.message}`);
+  console.log("Shutting down the server due to unhandled promise rejection");
+  server.close(() => {
+    process.exit(1);
+  });
 });
