d051f60976b30996c8f524ec8fadd17f849be707
KALASA_IVAN <moikalasa12@gmail.com>
2021-12-03 08:36:49 +0200

    Handling uncaught and mongoose object id errors

--- backend/middlewares/errors.js
+++ backend/middlewares/errors.js
@@ -15,6 +15,18 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
     error.message = err.message;
 
+    //wrong mongoose object id error
+    if (err.name === 'CastError') {
+      const message = `Resource not found. Invalid:${err.path}`
+      error=new ErrorHandler(message,400)
+    }
+
+    //Handling mongoose validation errors
+    if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map(value => value.message)
+      error=new ErrorHandler(message,400)
+    }
+
     res.status(error.statusCode).json({
       success: false,
       message: error.message || "Internal server error",
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,13 @@ const app = require("./app");
 
 const dotenv = require("dotenv");
 
+//Handling uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR:${err.stack}`);
+  console.log("Shutting down due to uncaught exception");
+  process.exit(1);
+});
+
 const connectDatabase = require("./config/database");
 
 // setting up config file
