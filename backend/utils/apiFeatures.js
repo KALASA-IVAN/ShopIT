2f2899a377187d30cf9bd4e3c069bababf395d5f
KALASA_IVAN <moikalasa12@gmail.com>
2021-12-06 22:30:00 +0200

    filtering search items

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -1,31 +1,32 @@
 class ApiFeatures {
   constructor(query, queryStr) {
-      this.query = query;
-      this.queryStr = queryStr;
-    }
-    search() {
-        const keyword = this.queryStr.keyword ? {
-            name: {
-                $regex: this.queryStr.keyword,
-                $options:'i'
-            }
-        } : {}
-        // console.log(keyword)
-        this.query = this.query.find({...keyword})
-        return this
-    }
-    filter() {
-        const queryCopy = { ...this.queryStr };
-        
-        //Removing fields from the copy
-        
-        const removeFields = ['keyword', 'limit', 'page'];
-        removeFields.forEach(el => delete queryCopy[el]);
-        // console.log(queryCopy);
-        this.query = this.query.find(queryCopy);
-        return this
-        
-    }
+    this.query = query;
+    this.queryStr = queryStr;
+  }
+  search() {
+    const keyword = this.queryStr.keyword
+      ? {
+          name: {
+            $regex: this.queryStr.keyword,
+            $options: "i",
+          },
+        }
+      : {};
+    // console.log(keyword)
+    this.query = this.query.find({ ...keyword });
+    return this;
+  }
+  filter() {
+    const queryCopy = { ...this.queryStr };
+
+    //Removing fields from the copy
+
+    const removeFields = ['keyword', 'limit', 'page'];
+    removeFields.forEach(el => delete queryCopy[el]);
+    // console.log(queryCopy);
+    this.query = this.query.find(queryCopy);
+    return this;
+  }
 }
 
 module.exports = ApiFeatures;
