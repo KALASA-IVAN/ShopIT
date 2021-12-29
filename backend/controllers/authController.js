cbc54148ddd9fe84bcbc01935f620767d1f7dc51
KALASA_IVAN <moikalasa12@gmail.com>
2021-12-30 01:05:21 +0200

    user authentication

---
+++ backend/controllers/authController.js
@@ -0,0 +1,23 @@
+const User = require('../models/user')
+const ErrorHandler = require('../utils/errorHandler')
+
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
+
+// Register a user  => /api/v1/register
+
+exports.registerUser = catchAsyncError(async (req, res, next) => {
+      
+    const { name, email, password } = req.body;
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        avatar: {
+            publicId: '',
+            url: ''
+        }
+    })
+
+
+ })
